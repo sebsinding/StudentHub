b82a0503ebf6bd8984f8f80857a98c561d528c43
miax6351 <miadong01@yahoo.com>
2022-03-16 13:50:21 +0100

    theme added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Layout from "./Components/Layouts/Layout";
 import Calendar from "./Pages/Calendar";
 import MyLessonPlan from "./Pages/MyLessonPlan";
@@ -9,9 +9,13 @@ import Grades from "./Pages/Grades";
 import NoPages from "./Pages/NoPages";
 import Login from "./Pages/Login";
 import AlternativeLayout from "./Components/Layouts/AlternativeLayout";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@mui/material";
+import customTheme from "./Pages/theme";
 
 export default function App() {
     return (
+      <ThemeProvider theme = {customTheme}>
       <BrowserRouter>
         <Routes>
           <Route path="/Login" element={<AlternativeLayout />} />
@@ -27,5 +31,6 @@ export default function App() {
           </Route>
         </Routes>
       </BrowserRouter>
+      </ThemeProvider>
     );
   };
--- src/Pages/Calendar.js
+++ src/Pages/Calendar.js
@@ -1,4 +1,3 @@
-import './Pages.css'; 
 import calendarImg from '../Resources/calendar.png';
 import * as React from 'react';
 import { useState } from 'react';
@@ -11,11 +10,16 @@ import {
   MonthView,
   Appointments,
 } from '@devexpress/dx-react-scheduler-material-ui';
+import { createTheme } from '@mui/material';
+
 
 const schedulerData = [
   { startDate: '2022-03-14T09:45', endDate: '2022-03-14T11:00', title: "Doctor's appointment" },
   { startDate: '2022-03-14T13:00', endDate: '2022-03-14T17:00', title: 'Front end web development' },
 ];
+
+
+
 
 const Calendar = () => {
   
@@ -23,8 +27,8 @@ const [currentDate, setCurrentDate] = useState('2022-03-14');
 
   return (
     <>    
-    <Paper>
-    <Scheduler
+    <Paper >
+    <Scheduler 
       data={schedulerData}
     >
       <ViewState
@@ -35,7 +39,8 @@ const [currentDate, setCurrentDate] = useState('2022-03-14');
         startDayHour={7}
         endDayHour={18}
       />
-      <Appointments />
+      <Appointments
+       />
       <Toolbar />
       <DateNavigator />
       <Appointments />
