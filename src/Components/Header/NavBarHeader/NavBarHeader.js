e26f13d5796528d8654c8fa768335fd331d3974c
Mia <miadong01@yahoo.com>
2022-03-29 13:36:31 +0200

    reload rerror in pages fixed

--- src/Components/Header/NavBarHeader/NavBarHeader.js
+++ src/Components/Header/NavBarHeader/NavBarHeader.js
@@ -1,5 +1,6 @@
 import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import './NavBarHeader.css';
+import { Link } from 'react-router-dom';
 
     // https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css
 
@@ -22,22 +23,22 @@ import './NavBarHeader.css';
             <>
             <Navbar sticky="top" variant="dark" className="NBHeader">
                 <Container>
-                    <Navbar.Brand href="/Calendar">StudentHub.</Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/Calendar">StudentHub.</Navbar.Brand>
                         <Nav>
-                            <Nav.Link className='navlink' href="/Calendar">Calendar</Nav.Link>
-                            <Nav.Link className='navlink' href="/MyLessonPlan">My Lesson Plan</Nav.Link>
-                            <Nav.Link className='navlink' href="/StudyGuidance">Study Guidance</Nav.Link>
-                            <Nav.Link className='navlink' href="/Announcements">Announcements</Nav.Link>
-                            <Nav.Link className='navlink' href="/CourseDatabase">Course Database</Nav.Link>
-                            <Nav.Link className='navlink' href="/Grades">Grades</Nav.Link>
+                            <Nav.Link as={Link} to="/Calendar" className='navlink'>Calendar</Nav.Link>
+                            <Nav.Link as={Link} to="/MyLessonPlan" className='navlink'>My Lesson Plan</Nav.Link>
+                            <Nav.Link as={Link} to="/StudyGuidance" className='navlink'>Study Guidance</Nav.Link>
+                            <Nav.Link as={Link} to="/Announcements" className='navlink'>Announcements</Nav.Link>
+                            <Nav.Link as={Link} to="/CourseDatabase" className='navlink'>Course Database</Nav.Link>
+                            <Nav.Link as={Link} to="/Grades" className='navlink'>Grades</Nav.Link>
                         </Nav>
                         <Nav>
                             <NavDropdown title="Pelle Andersen">
-                                <NavDropdown.Item href="">Settings</NavDropdown.Item>
-                                <NavDropdown.Item href="">hello</NavDropdown.Item>
-                                <NavDropdown.Item href="">:)</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/Calendar">Settings</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/Calendar">hello</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/Calendar">:)</NavDropdown.Item>
                                 <NavDropdown.Divider />
-                                <NavDropdown.Item href="/">Log out</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/">Log out</NavDropdown.Item>
                             </NavDropdown>
                         </Nav>
                 </Container>
