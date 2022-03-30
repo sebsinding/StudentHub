9043de51e2bb0fc36fcc6a7fad03e853c8fd5f7e
Mia <miadong01@yahoo.com>
2022-03-30 16:29:19 +0200

    tablecells

--- src/Pages/MyLessonPlan/MyLessonPlanPage.js
+++ src/Pages/MyLessonPlan/MyLessonPlanPage.js
@@ -12,11 +12,59 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import './Styles/LessonPlan.css';
+//Paper
+import Paper from '@mui/material/Paper';
+
+//Table
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
 
 const weeks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 const numberOfAccordions = [1, 2, 3];
 
 
+const AccordionTable = (
+  <div>
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+      >
+          <TableRow>
+          <TableCell>Date </TableCell>
+          <TableCell align="right">Topic</TableCell>
+          <TableCell align="right">Learning Objectives</TableCell>
+          <TableCell align="right">Litterature</TableCell>
+          <TableCell align="right">Pages</TableCell>
+          <TableCell align="right">Assignments</TableCell>
+        </TableRow>
+
+      </AccordionSummary>
+      <AccordionDetails>
+        <Typography>
+          Hello
+        </Typography>
+
+      </AccordionDetails>
+    </Accordion>
+  </div>
+);
+const TableRows = (
+  <div>
+          <TableRow>
+          <TableCell>Date</TableCell>
+          <TableCell align="right">Topic</TableCell>
+          <TableCell align="right">Learning Objectives</TableCell>
+          <TableCell align="right">Litterature</TableCell>
+          <TableCell align="right">Pages</TableCell>
+          <TableCell align="right">Assignments</TableCell>
+        </TableRow>
+  </div>
+);
 
 function skipLastTimelineItem(number) {
   if (number != weeks.length) {
@@ -31,57 +79,21 @@ const rowItems = weeks.map((number) =>
     <div>
       <TimelineItem>
         <TimelineSeparator>
-          <TimelineDot  />
+          <TimelineDot />
           {skipLastTimelineItem(number)}
         </TimelineSeparator>
         <TimelineContent>Week {number}</TimelineContent>
       </TimelineItem>
     </div>
+
     <div>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-        >
-          <Typography>Topic of today is..</Typography>
-
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-
-        </AccordionDetails>
-      </Accordion>
+      {TableRows}
     </div>
-    {/*{accordions}*/}
-  </div>
-);
 
-const accordions = numberOfAccordions.map(() =>
-
-  <div>
-    <Accordion>
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-      >
-        <Typography>Topic of today is..</Typography>
-
-      </AccordionSummary>
-      <AccordionDetails>
-        <Typography>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          malesuada lacus ex, sit amet blandit leo lobortis eget.
-        </Typography>
-
-      </AccordionDetails>
-    </Accordion>
+    {/*accordions*/}
   </div>
 
-
-
 );
-
 
 
 
@@ -89,9 +101,32 @@ const accordions = numberOfAccordions.map(() =>
 const MyLessonPlanPage = () => {
   return (
     <>
-     <Timeline position="left">
-        {rowItems}
-      </Timeline>
+
+        <div className="row">
+          
+          <div>
+          </div>
+          <Paper >
+          <div>
+          <TableRow>
+          <TableCell>Date </TableCell>
+          <TableCell align="right">Topic</TableCell>
+          <TableCell align="right">Learning Objectives</TableCell>
+          <TableCell align="right">Litterature</TableCell>
+          <TableCell align="right">Pages</TableCell>
+          <TableCell align="right">Assignments</TableCell>
+        </TableRow>
+          </div>
+          </Paper>
+        </div>
+      
+    
+        <div>
+          <Timeline position="left">
+            {rowItems}
+          </Timeline>
+        </div>
+       
     </>
   )
 };
