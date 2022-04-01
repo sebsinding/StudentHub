2dcc4351e82d32b01888b7072b05dd3ce79f2c70
s205345 <sebassinding@gmail.com>
2022-04-01 14:42:32 +0200

    study guidence - big commit

--- src/Pages/StudyGuidance.js
+++ src/Pages/StudyGuidance.js
@@ -1,7 +1,35 @@
+import './StudyGuidance.css';
+
+
+//TODO: sæt "send" button ind i textarea, fjern skygger fra knapper, skriv tekst i text area lav baggrundsfarve/billede
+
 const StudyGuidance = () => {
+  const openNav = () => {
+    document.getElementById("mySidepanel").style.width = "250px";
+  } 
+  const closeNav = () => {
+    document.getElementById("mySidepanel").style.width = "0";
+  } 
+  
+
     return (
-      <p>Her kommer StudyGuidance :)</p>
+    <>
+    <h1 className='Welcome'>Study Guidance</h1>
+    <div id="mySidepanel" class="sidepanel">
+      <a href="javascript:void(0)" class="closebtn" onClick={closeNav}>&times;</a>
+    </div>
+
+    <button class="openbtn" onClick={openNav}>Book a meeting</button>
+    <br></br>
+    <h4 className='minor_Tekst'>Number:  +45 45 25 11 99</h4>
+    <h6 className='minor_Tekst'>Email:  studvejl@adm.dtu.dk</h6>
+    <div class="textarea-container">
+      <textarea name="foo">Dear Study Guidance</textarea>
+      <button id = "send_btn">Send</button>
+    </div>
+    </>
     )
-  };
+  }
+  ;
   
   export default StudyGuidance;
